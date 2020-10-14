ce26386c8306e3371769a6b1a60a7e6af244aaac
rolbike4 <teodor_libita@yahoo.com>
2020-10-14 11:49:31 +0100

    first commit

---
+++ src/Components/Week 1/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const Week1 = () => {
+
+    const name ="Teo";
+
+    const location = () => "Fareham";
+
+    return (
+    <h1> 
+        Hello {name} You are in {location()}
+    </h1>
+    );
+};
+
+export default Week1;
