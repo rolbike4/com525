8ac66484757a95f8e1e6e3b840aa9aa14b8bfb98
rolbike4 <teodor_libita@yahoo.com>
2020-10-21 12:48:47 +0100

    week 3

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Week1 from "./Components/Week 1"
+import Week3 from "./Components/Week 3"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <>
+    <Week1/>
+    <Week3/>
+  </>
 
+);
 export default App;
---
+++ src/Components/Week 3/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import button from "C:\Users\teote\Desktop\interaction_design\test-folder\src\assets"
+
+const Week3 = () =>
+    <>
+        <img src={button} />
+    </>
+);
+
+export default Week3;
